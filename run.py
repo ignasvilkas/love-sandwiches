4ecf0de254529a336d0514f3584c10c6380ec0ab
ignasvilkas <ignas.pilybas@icloud.com>
2024-03-01 22:34:41 +0000

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -10,6 +11,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
   """
@@ -24,13 +26,15 @@ def get_sales_data():
   sales_data = data_str.split(",")
   validate_data(sales_data)
 
+
 def validate_data(values):
   """
   Inside the try, converts all string values into integers.
-  Raies ValueError if strings cannot be converted into int,
+  Raises ValueError if strings cannot be converted into int,
   or if there aren't exactly 6 values.
   """
   try:
+    [int(value) for value in values]
     if len(values) != 6:
       raise ValueError(
         f"Exactly 6 values required, your provided {len(values)}"
